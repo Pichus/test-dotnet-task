3c63fefb078398352af0ee38dbea2deb1346e792
pichus <dimatyzuk@gmail.com>
2025-07-23 16:05:20 +0300

    add ef core and configure a db context

---
+++ TestDotnetTask/Database/TestDotnetTaskContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TestDotnetTask.Database;
+
+public class TestDotnetTaskContext : DbContext
+{
+    
+}
--- TestDotnetTask/Program.cs
+++ TestDotnetTask/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TestDotnetTask.Database;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TestDotnetTaskContext>(
+    options => options.UseInMemoryDatabase("TestDotnetTaskDatabase"));
 
 builder.Services.AddControllers();
 
