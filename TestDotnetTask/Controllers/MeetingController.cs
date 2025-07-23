ea811d03ae47ce1aa750912e693575af2a3feac7
pichus <dimatyzuk@gmail.com>
2025-07-23 16:51:40 +0300

    add business hours validation for meeting controller

--- TestDotnetTask/Controllers/MeetingController.cs
+++ TestDotnetTask/Controllers/MeetingController.cs
@@ -10,6 +10,17 @@ public class MeetingController : ControllerBase
     [HttpPost]
     public async Task CreateMeeting(CreateMeetingRequest request)
     {
+    }
+
+    private bool IsCreateMeetingRequestValid(CreateMeetingRequest request)
+    {
+        const int businessHoursStartHour = 9;
+        const int businessHoursEndHour = 17;
         
+        bool isMeetingInBusinessHours = 
+            request.EarliestStart.TimeOfDay >= TimeSpan.FromHours(businessHoursStartHour) &&
+            request.LatestEnd.TimeOfDay <= TimeSpan.FromHours(businessHoursEndHour);
+
+        return isMeetingInBusinessHours;
     }
 }
